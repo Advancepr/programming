2c0ebd4f7ef7b3c71bb4f12cf006a4742ce9cb58
khan854 <shafiqullahafghan1234@gmail.com>
2022-10-16 01:19:15 -0700

    user class for the project

---
+++ user.java
@@ -0,0 +1,21 @@
+public class User {
+
+	/*
+	 The first name of the user
+	 */
+	private String FirstName;
+	
+	/*
+	 * the last name of the user
+	 */
+	private String LastName;
+	/*
+	 * the 10 number of the user
+	 */
+	private String uuid;
+	/*
+	 * the MD5 hash of the user's pin number
+	 */
+	private byte pinhash[];
+
+}
